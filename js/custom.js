2ebfd7d0dc03790985793c1ffc0ed513acd05e25
enkgare45 <enkgare45@gmail.com>
2017-12-16 03:03:25 -0500

    12-16-2017

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+
+	$('ul.dropdown').superfish({
+		autoArrows: true,
+		animation: {height:'show'}
+	});  
+
+});
+ $(document).ready(function(){
+    $( "#bookmodal" ).hover(function() {
+           $('.modal').modal({
+        show: true
+    });
+  });  
+});
